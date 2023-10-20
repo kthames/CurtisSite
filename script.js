1480a5bd7ff0795d62970416cdead030f4e28fca
kthames <135296109+kthames@users.noreply.github.com>
2023-10-20 17:59:56 -0500

    basic template

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const menuList = document.getElementById("menuList");
 
 menuList.style.maxHeight = "0px";
-menuList.innerHTML = ``; 
 
 function togglemenu(){
     if(menuList.style.maxHeight == "0px") 
     {
         menuList.style.maxHeight = "130px";
-        menuList.innerHTML = `<li><a href="">Listen</a></li>
-        <li><a href="">Follow</a></li>
-        <li><a href="">About</a></li>`
+        menuList.innerHTML = `<li><a href="">LISTEN</a></li>
+        <li><a href="">FOLLOW</a></li>
+        <li><a href="">ABOUT</a></li>`
     } else {
         menuList.style.maxHeight = "0px";
         menuList.innerHTML = ``;
